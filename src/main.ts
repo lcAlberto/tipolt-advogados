a8d6dd087c1d0ac0c5b25e105103232108c59407
lcAlberto <lcalbertomatoso2012@gmail.com>
2024-11-04 18:29:55 -0300

    added google analytics

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,21 @@ import './assets/fonts.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { FontAwesomeIcon } from '@/plugins/FontAwesome'
+import VueGtagPlugin from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
+import * as process from 'node:process'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(VueGtagPlugin, {
+  config: { id: process.env.VUE_APP_METRIC_ID },
+  pageTrackerScreenView: true,
+});
+
 
 app.mount('#app')
