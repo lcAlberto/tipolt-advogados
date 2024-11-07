a181ac2410aea03c7fa732cd1f45855bb8fe0192
lcAlberto <lcalbertomatoso2012@gmail.com>
2024-11-07 15:22:02 -0300

    change gtag script

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import './assets/fonts.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { FontAwesomeIcon } from '@/plugins/FontAwesome'
-import VueGtagPlugin from 'vue-gtag'
+import gtmPlugin from './plugins/gTag';
+// import VueGtagPlugin from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -14,10 +15,12 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.component('font-awesome-icon', FontAwesomeIcon)
-app.use(VueGtagPlugin, {
-  config: { id: import.meta.env.VITE_API_METRIC_ID },
-  pageTrackerScreenviewEnabled: true,
+app.use(gtmPlugin, {
+  id: import.meta.env.VITE_API_METRIC_ID
 });
-console.log(import.meta.env.VITE_API_METRIC_ID)
+// app.use(VueGtagPlugin, {
+//   config: { id: import.meta.env.VITE_API_METRIC_ID },
+//   pageTrackerScreenviewEnabled: true,
+// });
 
 app.mount('#app')
